b8f55654691e6f41f3142adff38d867527a3f59c
Ehsan Sarboukh <Ehsan Sarboukh@LAPTOP-2UID0TPR>
2022-11-27 18:36:23 +0200

    Name + last name

--- Hw1Method/src/prototype/Client.java
+++ Hw1Method/src/prototype/Client.java
@@ -1,8 +1,11 @@
 package prototype;
 
 public class Client {
-	public Client()
+	String name;
+	String lName;
+	public Client(String name,String lName)
 	{
-		
+		this.name=name;
+		this.lName=lName;
 	}
 }
