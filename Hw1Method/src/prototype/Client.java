adbac4cffc2bd2a14546db74afb0c886d2cb1523
ThinkPad <sabeel.hamood@e.braude.ac.il>
2022-11-27 18:38:43 +0200

    This is Sabeel hi

--- Hw1Method/src/prototype/Client.java
+++ Hw1Method/src/prototype/Client.java
@@ -3,9 +3,13 @@ package prototype;
 public class Client {
 	String name;
 	String lName;
-	public Client(String name,String lName)
-	{
+	int id;
+	public Client(String Name,String lName,int id)
+	{	
 		this.name=name;
 		this.lName=lName;
+		this.id=id;
+		
 	}
+	
 }
